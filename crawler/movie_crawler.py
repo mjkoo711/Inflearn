a2568294b66dc8faa78977eda380d268e237f200
KooMinJun <mjkoo711@gmail.com>
2019-11-17 22:38:31 +0900

    알리미에 텔레그램 봇 추가하기

--- crawler/movie_crawler.py
+++ crawler/movie_crawler.py
@@ -1,8 +1,10 @@
 #-*- coding: utf-8 -*-
 
 import requests
+import telegram
 from bs4 import BeautifulSoup
 
+bot = telegram.Bot(token='999630453:AAGjO2fijWMeWmCYMt6kFw5-C6IKJ8UINtY')
 url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20191123'
 html = requests.get(url)
 
@@ -11,7 +13,8 @@ imax = soup.select_one('span.imax')
 if (imax) :
     imax = imax.find_parent('div', class_='col-times')
     title = imax.select_one('div.info-movie > a > strong').text.strip()
-    
-    print(title + '의 IMAX 예매가 열렸습니다.')
+    bot.sendMessage(chat_id=967963705, text=title + ' IMAX 예매가 열렸습니다.')
+    #print(title + '의 IMAX 예매가 열렸습니다.')
 else :
-    print('IMAX 예매가 열리지 않았습니다.')
+    bot.sendMessage(chat_id=967963705, text='IMAX 예매가 열리지 않았습니다.')
+
